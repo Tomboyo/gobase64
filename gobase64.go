3f6f91abc22bfc3ab6b7fbe9c5273a3361c15356
harrjdk <harrj902@protonmail.com>
2022-03-06 17:53:26 -0500

    add concurrent base64 support

--- gobase64.go
+++ gobase64.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/list"
 	"encoding/binary"
 	"errors"
 	"io"
@@ -8,6 +9,13 @@ import (
 	"math/bits"
 	"os"
 )
+
+// our worker count
+const WORKERS = 4
+
+type B64Chars struct {
+	value []byte
+}
 
 // Not immutable but runtime array slicing to create a pseudo-immutable array is slower
 var charset = [64]byte{
@@ -18,15 +26,15 @@ var charset = [64]byte{
 
 // sextuple to chars
 // assumes output is initially empty
-func getChars(buffer *[]byte, padding int) (string, error) {
+func getChars(buffer *[]byte, padding int) ([]byte, error) {
 	if padding > 2 {
-		return "", nil
+		return []byte{}, nil
 	}
-	var out string
+	var out []byte
 	var in = *buffer
 	var l = len(in)
 	if l != 3 {
-		return "", errors.New("expected 3 bytes")
+		return nil, errors.New("expected 3 bytes")
 	}
 	if padding > 0 {
 		in[2] = 0x00
@@ -36,23 +44,58 @@ func getChars(buffer *[]byte, padding int) (string, error) {
 	}
 	if l > 0 {
 		var unsigned = binary.BigEndian.Uint32(append([]byte{0x00}, in...))
-		var part1, part2, part3, part4 string
-		if padding > 0 {
-			part1 = ""
-		} else {
-			part1 = string(charset[(unsigned & 0x003F)])
+		out = append(out, charset[bits.RotateLeft32((unsigned&0xFC0000), -18)])
+		out = append(out, charset[bits.RotateLeft32((unsigned&0x3F000), -12)])
+		if padding < 2 {
+			out = append(out, charset[bits.RotateLeft32((unsigned&0x0FC0), -6)])
 		}
-		if padding > 1 {
-			part2 = ""
-		} else {
-			part2 = string(charset[bits.RotateLeft32((unsigned&0x0FC0), -6)])
+		if padding < 1 {
+			out = append(out, charset[(unsigned&0x003F)])
 		}
-		part3 = string(charset[bits.RotateLeft32((unsigned&0x3F000), -12)])
-		part4 = string(charset[bits.RotateLeft32((unsigned&0xFC0000), -18)])
-		out = part4 + part3 + part2 + part1
 	}
 	return out, nil
 }
+
+func btoa_parallel(reader io.Reader, writer io.Writer) {
+	c := make(chan *B64Chars, 16)
+
+	// load 3^10 bytes
+	var inbuf = make([]byte, 3)
+	var bl = 0
+	outbuffer := list.New()
+	outbuffer.Init()
+	for {
+		r, rerr := reader.Read(inbuf)
+		bl += r
+		if rerr != nil && rerr != io.EOF {
+			log.Fatal("Unexpected IO error", rerr)
+		}
+		// offload the getChars to a byte array
+		go func() {
+			s, err := getChars(&inbuf, 3-r)
+			if err != nil {
+				log.Fatal("Unexpected Base64 encoding error", err)
+			}
+			chars := new(B64Chars)
+			chars.value = s
+			c <- chars
+		}()
+		outbuffer.PushBack(<-c)
+		if rerr == io.EOF {
+			break
+		}
+	}
+	for e := outbuffer.Front(); e != nil; e = e.Next() {
+		writer.Write(e.Value.(*B64Chars).value)
+	}
+	var p = (bl % 3)
+	if p == 1 {
+		writer.Write([]byte{'=', '='})
+	} else if p == 2 {
+		writer.Write([]byte{'='})
+	}
+}
+
 func btoa(reader io.Reader, writer io.Writer) {
 	var inbuf = make([]byte, 3)
 	var bl = 0
@@ -66,7 +109,7 @@ func btoa(reader io.Reader, writer io.Writer) {
 		if err != nil {
 			log.Fatal("Unexpected Base64 encoding error", err)
 		}
-		writer.Write([]byte(out))
+		writer.Write(out)
 		if rerr == io.EOF {
 			break
 		}
@@ -82,6 +125,15 @@ func btoa(reader io.Reader, writer io.Writer) {
 func main() {
 	var reader = os.Stdin
 	var writer = os.Stdout
-
-	btoa(reader, writer)
+	rawArgs := os.Args
+	if len(rawArgs) > 1 {
+		args := rawArgs[1:]
+		if args[0] == "p" {
+			btoa_parallel(reader, writer)
+		} else {
+			log.Fatal("Unknown flag", args[0])
+		}
+	} else {
+		btoa(reader, writer)
+	}
 }
