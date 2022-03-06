f7ca7c80ab8b787f4386b5e6b60c98fa9854c82c
harrjdk <harrj902@protonmail.com>
2022-03-06 10:36:13 -0500

    fix padding support

--- gobase64.go
+++ gobase64.go
@@ -19,22 +19,31 @@ var charset = [64]byte{
 // sextuple to chars
 // assumes output is initially empty
 func getChars(buffer *[]byte, padding int) (string, error) {
+	if padding > 2 {
+		return "", nil
+	}
 	var out string
 	var in = *buffer
 	var l = len(in)
 	if l != 3 {
 		return "", errors.New("expected 3 bytes")
 	}
+	if padding > 0 {
+		in[2] = 0x00
+	}
+	if padding > 1 {
+		in[1] = 0x00
+	}
 	if l > 0 {
 		var unsigned = binary.BigEndian.Uint32(append([]byte{0x00}, in...))
 		var part1, part2, part3, part4 string
 		if padding > 0 {
-			part1 = "="
+			part1 = ""
 		} else {
 			part1 = string(charset[(unsigned & 0x003F)])
 		}
 		if padding > 1 {
-			part2 = "="
+			part2 = ""
 		} else {
 			part2 = string(charset[bits.RotateLeft32((unsigned&0x0FC0), -6)])
 		}
@@ -46,19 +55,27 @@ func getChars(buffer *[]byte, padding int) (string, error) {
 }
 func btoa(reader io.Reader, writer io.Writer) {
 	var inbuf = make([]byte, 3)
-
+	var bl = 0
 	for {
-		r, err := reader.Read(inbuf)
-		if err == io.EOF {
-			break
-		} else if err != nil {
-			log.Fatal("Unexpected IO error", err)
+		r, rerr := reader.Read(inbuf)
+		bl += r
+		if rerr != nil && rerr != io.EOF {
+			log.Fatal("Unexpected IO error", rerr)
 		}
 		out, err := getChars(&inbuf, 3-r)
 		if err != nil {
 			log.Fatal("Unexpected Base64 encoding error", err)
 		}
 		writer.Write([]byte(out))
+		if rerr == io.EOF {
+			break
+		}
+	}
+	var p = (bl % 3)
+	if p == 1 {
+		writer.Write([]byte{'=', '='})
+	} else if p == 2 {
+		writer.Write([]byte{'='})
 	}
 }
 
